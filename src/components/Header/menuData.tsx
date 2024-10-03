9b2ece1ef270495cb6b5a5c319e1e662df28440d
Anuj Sharma <anujshama4u@gmail.com>
2024-10-03 12:31:31 +0530

    Initial commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Features",
+    path: "/#features",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "How it works",
+    path: "/#how-it-works",
+    newTab: false,
+  },
+  {
+    id: 4,
+    title: "Pricing",
+    path: "/#pricing",
+    newTab: false,
+  },
+];
+export default menuData;
